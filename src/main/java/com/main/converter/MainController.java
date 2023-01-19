4646b88f3f232a5f4fb6b1f5bec10f58e07ac3fa
p20100 <71640994+Lilly05@users.noreply.github.com>
2023-01-19 15:57:39 +0100

    i wrote now all the code to convert the number (no checking of the input yet tho)

--- src/main/java/com/main/converter/MainController.java
+++ src/main/java/com/main/converter/MainController.java
@@ -106,24 +106,63 @@ public class MainController implements Initializable {
                     break;
                 }
                 case "Hexadecimal": {
-                    String hexNumber = input.getText();
+                    String number = input.getText().toUpperCase();
                     int savedNumber = 0;
                     StringBuilder stringBuilder = new StringBuilder();
-                    stringBuilder.append(hexNumber);
+                    stringBuilder.append(number);
                     String reversedNumber = String.valueOf(stringBuilder.reverse());
-                    for (int i = 0; i < reversedNumber.length(); i++){
+                    for (int i = 0; i < reversedNumber.length(); i++) {
                         char c = reversedNumber.charAt(i);
-                        int eachNumber = Character.getNumericValue(c);
-                        savedNumber += eachNumber * Math.pow(16, i);
+                        switch (c){
+                            case 'A': {
+                                savedNumber += 10 * Math.pow(16, i);
+                                break;
+                            }
+                            case 'B': {
+                                savedNumber += 11 * Math.pow(16, i);
+                                break;
+                            }
+                            case 'C': {
+                                savedNumber += 12 * Math.pow(16, i);
+                                break;
+                            }
+                            case 'D': {
+                                savedNumber += 13 * Math.pow(16, i);
+                                break;
+                            }
+                            case 'E': {
+                                savedNumber += 14 * Math.pow(16, i);
+                                break;
+                            }
+                            case 'F': {
+                                savedNumber += 15 * Math.pow(16, i);
+                                break;
+                            }
+                            default: {
+                                savedNumber += Character.getNumericValue(c) * Math.pow(16, i);
+                            }
+                        }
                     }
                     convertedNumber = String.valueOf(savedNumber);
                     break;
                 }
                 case "Octal": {
-
+                    int number = Integer.parseInt(input.getText());
+                    int savedNumber = 0;
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.append(number);
+                    String reversedNumber = String.valueOf(stringBuilder.reverse());
+                    for (int i = 0; i < reversedNumber.length(); i++){
+                        char c = reversedNumber.charAt(i);
+                        int eachNumber = Character.getNumericValue(c);
+                        savedNumber += eachNumber * Math.pow(8, i);
+                    }
+                    convertedNumber = String.valueOf(savedNumber);
+                    break;
                 }
                 default: {
-
+                    int number = Integer.parseInt(input.getText());
+                    convertedNumber = String.valueOf(number);
                 }
             }
 
@@ -145,13 +184,55 @@ public class MainController implements Initializable {
                     break;
                 }
                 case "Hexadecimal": {
-
+                    int reminder = Integer.parseInt(convertedNumber);
+                    convertedNumber = "";
+                    while(reminder != 0){
+                            System.out.println(reminder);
+                            int reminderOfModulo = reminder % 16;
+                            switch (reminderOfModulo){
+                                case 10: {
+                                    convertedNumber += "A"; break;
+                                }
+                                case 11: {
+                                    convertedNumber += "B"; break;
+                                }
+                                case 12: {
+                                    convertedNumber += "C"; break;
+                                }
+                                case 13: {
+                                    convertedNumber += "D"; break;
+                                }
+                                case 14: {
+                                    convertedNumber += "E"; break;
+                                }
+                                case 15: {
+                                    convertedNumber += "F"; break;
+                                }
+                                default:{
+                                    convertedNumber += reminderOfModulo;
+                                }
+                            }
+                        reminder = reminder / 16;
+                    }
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.append(convertedNumber);
+                    result.setText(String.valueOf(stringBuilder.reverse()));
+                    break;
                 }
                 case "Octal": {
-
+                    int reminder = Integer.parseInt(convertedNumber);
+                    convertedNumber = "";
+                    while(reminder != 0){
+                        convertedNumber += reminder % 8;
+                        reminder = reminder / 8;
+                    }
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.append(convertedNumber);
+                    result.setText(String.valueOf(stringBuilder.reverse()));
+                    break;
                 }
                 default: {
-
+                    result.setText(convertedNumber);
                 }
             }
         } catch (Exception e){
