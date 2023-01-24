c278a4d908520922c5bc9e679b4351d14834ad60
p20100 <71640994+Lilly05@users.noreply.github.com>
2023-01-24 14:49:52 +0100

    finished the converter

--- src/main/java/com/main/converter/MainController.java
+++ src/main/java/com/main/converter/MainController.java
@@ -97,12 +97,17 @@ public class MainController implements Initializable {
                     StringBuilder stringBuilder = new StringBuilder();
                     stringBuilder.append(number);
                     String reversedNumber = String.valueOf(stringBuilder.reverse());
-                    for (int i = 0; i < reversedNumber.length(); i++){
-                        char c = reversedNumber.charAt(i);
-                        int eachNumber = Character.getNumericValue(c);
-                        savedNumber += eachNumber * Math.pow(2, i);
+                    if (checkIfNumberIsBinary(reversedNumber)) {
+                        for (int i = 0; i < reversedNumber.length(); i++) {
+                            char c = reversedNumber.charAt(i);
+                            int eachNumber = Character.getNumericValue(c);
+                            savedNumber += eachNumber * Math.pow(2, i);
+                        }
+                        convertedNumber = String.valueOf(savedNumber);
+                    }else{
+                        result.setText("That's not a valid number");
+                        return;
                     }
-                    convertedNumber = String.valueOf(savedNumber);
                     break;
                 }
                 case "Hexadecimal": {
@@ -111,39 +116,44 @@ public class MainController implements Initializable {
                     StringBuilder stringBuilder = new StringBuilder();
                     stringBuilder.append(number);
                     String reversedNumber = String.valueOf(stringBuilder.reverse());
-                    for (int i = 0; i < reversedNumber.length(); i++) {
-                        char c = reversedNumber.charAt(i);
-                        switch (c){
-                            case 'A': {
-                                savedNumber += 10 * Math.pow(16, i);
-                                break;
-                            }
-                            case 'B': {
-                                savedNumber += 11 * Math.pow(16, i);
-                                break;
-                            }
-                            case 'C': {
-                                savedNumber += 12 * Math.pow(16, i);
-                                break;
-                            }
-                            case 'D': {
-                                savedNumber += 13 * Math.pow(16, i);
-                                break;
-                            }
-                            case 'E': {
-                                savedNumber += 14 * Math.pow(16, i);
-                                break;
-                            }
-                            case 'F': {
-                                savedNumber += 15 * Math.pow(16, i);
-                                break;
-                            }
-                            default: {
-                                savedNumber += Character.getNumericValue(c) * Math.pow(16, i);
+                    if (checkIfNumberIsHexadecimal(reversedNumber)) {
+                        for (int i = 0; i < reversedNumber.length(); i++) {
+                            char c = reversedNumber.charAt(i);
+                            switch (c) {
+                                case 'A': {
+                                    savedNumber += 10 * Math.pow(16, i);
+                                    break;
+                                }
+                                case 'B': {
+                                    savedNumber += 11 * Math.pow(16, i);
+                                    break;
+                                }
+                                case 'C': {
+                                    savedNumber += 12 * Math.pow(16, i);
+                                    break;
+                                }
+                                case 'D': {
+                                    savedNumber += 13 * Math.pow(16, i);
+                                    break;
+                                }
+                                case 'E': {
+                                    savedNumber += 14 * Math.pow(16, i);
+                                    break;
+                                }
+                                case 'F': {
+                                    savedNumber += 15 * Math.pow(16, i);
+                                    break;
+                                }
+                                default: {
+                                    savedNumber += Character.getNumericValue(c) * Math.pow(16, i);
+                                }
                             }
                         }
+                        convertedNumber = String.valueOf(savedNumber);
+                    }else{
+                        result.setText("That's not a valid number");
+                        return;
                     }
-                    convertedNumber = String.valueOf(savedNumber);
                     break;
                 }
                 case "Octal": {
@@ -152,12 +162,17 @@ public class MainController implements Initializable {
                     StringBuilder stringBuilder = new StringBuilder();
                     stringBuilder.append(number);
                     String reversedNumber = String.valueOf(stringBuilder.reverse());
-                    for (int i = 0; i < reversedNumber.length(); i++){
-                        char c = reversedNumber.charAt(i);
-                        int eachNumber = Character.getNumericValue(c);
-                        savedNumber += eachNumber * Math.pow(8, i);
+                    if (checkIfNumberIsOctal(reversedNumber)) {
+                        for (int i = 0; i < reversedNumber.length(); i++) {
+                            char c = reversedNumber.charAt(i);
+                            int eachNumber = Character.getNumericValue(c);
+                            savedNumber += eachNumber * Math.pow(8, i);
+                        }
+                        convertedNumber = String.valueOf(savedNumber);
+                    }else{
+                        result.setText("That's not a valid number");
+                        return;
                     }
-                    convertedNumber = String.valueOf(savedNumber);
                     break;
                 }
                 default: {
@@ -187,7 +202,6 @@ public class MainController implements Initializable {
                     int reminder = Integer.parseInt(convertedNumber);
                     convertedNumber = "";
                     while(reminder != 0){
-                            System.out.println(reminder);
                             int reminderOfModulo = reminder % 16;
                             switch (reminderOfModulo){
                                 case 10: {
@@ -238,5 +252,34 @@ public class MainController implements Initializable {
         } catch (Exception e){
             result.setText("That's not a valid number");
         }
+    }
+
+    public boolean checkIfNumberIsBinary(String input){
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != '1' && input.charAt(i) != '0') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean checkIfNumberIsHexadecimal(String input){
+        for (int i = 0; i < input.length(); i++){
+            if (!Character.isDigit(input.charAt(i))){
+                if(input.charAt(i) != 'A' && input.charAt(i) != 'B' && input.charAt(i) != 'C' && input.charAt(i) != 'D' && input.charAt(i) != 'E' && input.charAt(i) != 'F'){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean checkIfNumberIsOctal(String input){
+        for (int i = 0; i < input.length(); i++) {
+            if (Integer.parseInt(String.valueOf(input.charAt(i))) > 7){
+                return false;
+            }
+        }
+        return true;
     }
 }
